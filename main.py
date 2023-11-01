a116dc4d23872d22d293a65f2c56f3372e25d018
Claudia Rodrigues <claudia.raphael73@gmail.com>
2023-11-01 09:54:20 -0300

    try first message

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import openai
+# retorna a resposta que esta dentro do dicionario resposta que contem a key "choices" ~com a resposta dentro de uma lista (ta na documentação)
+# https://platform.openai.com/docs/guides/gpt/chat-completions-api
+
 
 openai_api_key = "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"
 # passa os dados da api pra Openai pra poder consumir os dados da api
@@ -7,11 +10,12 @@ openai.api_key = openai_api_key
 
 def send_message(message):
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo"
+        model = "gpt-3.5-turbo",
         messages = [
-            {"role": "user", "content": mensagem}
+            {"role": "user", "content": message}
         ]
     )
 
-    return response["choices"][0]
-# retorna a resposta que esta dentro do dicionario resposta que contem a key "choices" ~com a resposta
+    return response["choices"][0]["message"]
+
+print(send_message("What's the medium price for weed in Toronto?"))
