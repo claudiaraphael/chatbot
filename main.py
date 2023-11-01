9ead5d55ecdaf9f19da51df8c3e671278b09a60c
Claudia Rodrigues <claudia.raphael73@gmail.com>
2023-11-01 09:32:05 -0300

    use api_key function form openai to call openai to consume api data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import openai
 
-api_key = "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"
+openai_api_key = "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"
+# passa os dados da api pra Openai pra poder consumir os dados da api
+
+openai.api_key = openai_api_key
