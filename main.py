4d4ca79592c0bb8ce8b421e945d4b4048307aad4
Claudia Rodrigues <claudia.raphael73@gmail.com>
2023-11-01 09:36:57 -0300

    create parameters for message function

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ openai_api_key = "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"
 # passa os dados da api pra Openai pra poder consumir os dados da api
 
 openai.api_key = openai_api_key
+
+def send_message(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo"
+        messages = [
+            {"role": "user", "content": mensagem}
+        ]
+    
+    
+    
+    )
